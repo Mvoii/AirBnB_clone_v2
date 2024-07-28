70dbe569f492060be744994b0bf43194042bcae0
Franklin Mvoi <mvoifranklin@gmail.com>
2024-07-28 23:21:47 +0300

    fixestask 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,7 +40,7 @@ def num_template(n=None):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even_template(n=None):
     """Render a html file"""
-    return render_template('6-number_odd_or_even.html',n=n)
+    return render_template('6-number_odd_or_even.html', n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
